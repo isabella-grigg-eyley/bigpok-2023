0fa389f14e6aeda47547801deb8fdce20f2e3dfc
isabella-grigg-eyley <isabella.grigg.eyley@gmail.com>
2023-05-14 16:38:56 +1200

    Add credits and music

--- UldenYueInstaller/Assets/CreditsScroller.cs
+++ UldenYueInstaller/Assets/CreditsScroller.cs
@@ -11,17 +11,23 @@ public class CreditsScroller : MonoBehaviour
 
     public RectTransform m_background = null;
 
+    public AudioSource m_rickroll = null;
+
     public float m_scrollTime = 10.0f;
     
     // Start is called before the first frame update
     void Start()
     {
         // Start music
-        ScrollCredits();
+        m_rickroll.Play();
+        StartCoroutine(ScrollCredits());
     }
 
-    private void ScrollCredits()
+    private IEnumerator ScrollCredits()
     {
+        // wait a few seconds
+        yield return new WaitForSeconds(5.0f);
+        
         float distanceToScroll = m_creditsText.rect.y + m_background.rect.y;
 
         Vector3 targetPos = m_creditsText.localPosition;
@@ -40,5 +46,6 @@ public class CreditsScroller : MonoBehaviour
     {
         print("credits done");
         // fade out music
+        DOTween.To(() => m_rickroll.volume, x => m_rickroll.volume = x, 0.0f, 5.0f);
     }
 }
